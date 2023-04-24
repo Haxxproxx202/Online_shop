674a1863733edcb6487d3db3263468411343f4af
Haxxproxx202 <pjanecki88@gmail.com>
2023-04-24 23:22:03 +0200

    own view in '/admin/' site

--- backend/orders/views.py
+++ backend/orders/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from cart.cart import Cart
 from .forms import OrderCreateForm
-from .models import OrderItem
+from .models import OrderItem, Order
 from .tasks import order_created
+from django.contrib.admin.views.decorators import staff_member_required
 
 
 def order_create(request):
@@ -27,5 +28,8 @@ def order_create(request):
     return render(request, 'orders/order/create.html', {'form': form, 'cart': cart})
 
 
-
-
+@staff_member_required
+# checks if is_active and is_staff if True
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request, 'admin/orders/order/detail.html', {'order': order})
